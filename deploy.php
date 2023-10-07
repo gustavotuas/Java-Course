80f11739986101ba58027ce30a960f0f89e7d8de
Gustavo Tua <gustavo.tua@hotmail.com>
2023-10-07 14:13:00 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -8,20 +8,11 @@ GT
 <?php
 echo "2023";
 //$output = shell_exec(" git branch");
-$output = shell_exec('/usr/bin/git -C /var/www/html/Java-Course pull 2>&1');
+//$output = shell_exec('/usr/bin/git -C /var/www/html/Java-Course pull 2>&1');
 $output2 = shell_exec('git --version');
-echo "<pre>$output</pre>"; 
+echo "<pre>$output2</pre>"; 
 ?>
-<?php
-echo "<br>------------------";
-// Set the path to your Git repository
-// Execute the git pull command
-$output2 = shell_exec('git pull');
-//$output2 = exec('sudo cd /var/www/html/Java-Course && git pull');
-// Display the output (optional)
-echo "<pre>1 $output2</pre>";
 
-?>
 
 </body>
 </html>
