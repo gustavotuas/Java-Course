19dbd5277c3d582927784d9d4ac2ec4ce4636e7f
Gustavo Tua <gustavo.tua@hotmail.com>
2023-10-07 13:38:04 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,8 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Deploy</title>
+</head>
+<body>
+GT
 <?php
-// Get the request data
-
-    // Pull the latest changes from the Git repository
-    exec("cd /var/www/html/Java-Course && git pull");
-    echo "Git pull successful!";
+echo "2023";
+$output = shell_exec("ls -l");
+echo "<pre>$output</pre>"; 
+?>
+<?php
+echo "<br>------------------";
+// Set the path to your Git repository
+// Execute the git pull command
+$output = shell_exec('git pull');
+//$output2 = exec('sudo cd /var/www/html/Java-Course && git pull');
+// Display the output (optional)
+echo "<pre>1 $output</pre>";
 
 ?>
+
+</body>
+</html>
