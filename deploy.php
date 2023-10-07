4b6152e948461d2a038baefccd16ec9da5e7dc32
Gustavo Tua <gustavo.tua@hotmail.com>
2023-10-07 02:11:25 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,22 +1,14 @@
 <?php
 // Get the request data
-$data = json_decode(file_get_contents("php://input"));
-
-// Verify that the payload is from GitHub (optional, but recommended)
-$secret = “DigitalOcean”; // Replace with your secret
-$hubSignature = $_SERVER['HTTP_X-Hub-Signature'] ?? '';
-list($algo, $hash) = explode('=', $hubSignature, 2);
-
-// Calculate the hash
-$payload = file_get_contents("php://input");
-$payloadHash = hash_hmac($algo, $payload, $secret);
-
-// Verify the signature
-if ($hash === $payloadHash) {
-    // Pull the latest changes from the Git repository
-    exec("cd /var/www/html/gustavotua/github/Java-Course && git pull");
-    echo "Git pull successful!";
-} else {
-    echo "Invalid signature.";
-}
+    $tmp = shell_exec("git pull");
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Deploy</title>
+</head>
+<body>
+<?php echo  $tmp; ?>
+</body>
+</html>
