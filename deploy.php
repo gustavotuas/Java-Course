27bdef4e3d0142e150d2e47d2d269aed15a4b14c
Gustavo Tua <gustavo.tua@hotmail.com>
2023-10-07 14:24:20 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -11,8 +11,10 @@ echo "<b>GitHub Deploy</b>";
 //$output = shell_exec('/usr/bin/git -C /var/www/html/Java-Course pull 2>&1');
 $output2 = shell_exec('git --version');
 $output = shell_exec('git branch');
+$output3 = shell_exec('git init');
 echo "<pre>$output2</pre>"; 
 echo "<pre>$output</pre>"; 
+echo "<pre>$output3</pre>"; 
 ?>
 
 
