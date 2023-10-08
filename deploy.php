8457a944cbab68de087859645373f2759a921e86
Gustavo Tua <gustavo.tua@hotmail.com>
2023-10-07 23:34:16 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?php
 
-$repositoryPath = '/var/www/html/Java-Course';
-$branch = 'master';
+//$repositoryPath = '/var/www/html/Java-Course';
+//$branch = 'master';
 
 // Get the request data
 $data = json_decode(file_get_contents("php://input"));
@@ -18,7 +18,7 @@ $payloadHash = hash_hmac($algo, $payload, $secret);
 // Verify the signature
 if ($hash === $payloadHash) {
     // Pull the latest changes from the Git repository
-    exec("git pull origin master");
+    exec("git pull");
     echo "Git pull successful!!";
 } else {
     echo "Invalid signature.";
