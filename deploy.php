c12a4ab6f1c4d58ec89c86c843f87b964179f4c1
Gustavo Tua <gustavo.tua@hotmail.com>
2023-10-08 02:00:05 -0500

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,12 +1,27 @@
 <?php
+
+echo "GIT PULL FROM DEVELOPMENT BRANCH";
+echo "<br>";
+// Checks start
+if(function_exists('exec')) {
+    echo "exec is enabled";
+}
+
+echo "<br>";
+echo exec('whoami');
+echo "<br>";
+echo exec('which git');
+echo "<br>";
+// Checks end
+
 // Define the path to your Git repository
 $repositoryPath = '/var/www/html/Java-Course';
 
 // Define the command to perform the Git pull
-$gitPullCommand = "git -C $repositoryPath pull";
+$gitPullCommand = "pwd && ls -a && git --version &&  git pull origin master 2>&1";
 
 // Execute the Git pull command using SSH agent
-$sshAgentCommand = "eval \$(ssh-agent -s) && ssh-add ~/.ssh/id_rsa && $gitPullCommand 2>&1";
+$sshAgentCommand = "eval \$(ssh-agent -s) && ssh-add ~/.ssh/id_rsa && $gitPullCommand ";
 
 // Execute the SSH agent command
 $output = exec($sshAgentCommand);
